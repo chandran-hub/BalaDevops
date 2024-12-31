1d97b7b2aa3f7835987a75a2213556626c07d00e
chandran-hub <balachandrank1708@gmail.com>
2024-12-31 08:21:15 +0530

    Update heloworld.java

--- heloworld.java
+++ heloworld.java
@@ -1,1 +1,19 @@
-djfdugyfydvjfsnuhsufbfgydfbcug
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    private Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(1, calculator.subtract(3, 2));
+    }
+}
